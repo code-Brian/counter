2f3dbb81e3f3e9bd3849a6c76d67ef42975d5192
Brian Denmark <testingarea479@gmail.com>
2022-08-22 21:26:45 -0700

    updated file structre

--- server.py
+++ server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/destroy_session')
+def destroy():
+    return render_template("destroy.html")
+
 if __name__ == '__main__':
     app.run(debug=True)

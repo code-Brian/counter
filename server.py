0c7361b54159027b7cb709de4f5fa5d74b38cb4c
Brian Denmark <testingarea479@gmail.com>
2022-08-23 21:05:06 -0700

    added session.pop for both routes

--- server.py
+++ server.py
@@ -12,6 +12,13 @@ def index():
         session['visits'] += 1
         print('------------ else statement')
 
+    if ('predators' not in session):
+        session['predators'] = 1
+        print('------------ predator if statement')
+    else:
+        session['predators'] += 1
+        print('------------ predator else statement')
+
     return render_template("index.html")
 
 @app.route('/visit', methods=['POST'])
@@ -20,7 +27,22 @@ def count_visits():
     session['visits'] = int(request.form['visits']) + session['visits'] -1
     print("--------------",type(session['visits']))
     return redirect('/')
+
+@app.route('/predators', methods=['POST'])
+def count_predators():
+    print('---------/visit route ran')
+    session['predators'] = int(request.form['predators']) + session['predators'] -1
+    print("--------------",type(session['predators']))
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.pop('visits')
+    session.pop('predators')
+    
+    return redirect('/')
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)     
 
 # session is a dictionary and so is request.form
